c1f432eb73d1d3119d96f8b18c0663a2b39140bf
Moses Kamara <kamara.moses@yahoo.com>
2021-03-19 20:49:55 -0700

    overhauled the styles.css styling

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,21 +2,22 @@ import React from 'react';
 import './styles.css';
 import { Card } from 'semantic-ui-react';
 import moment from 'moment';
+import { DivMode } from 'tsparticles';
+import { DivType } from 'tsparticles';
 
 
 const CardExampleCard = ({weatherData}) => (
-  <Card>
-    <Card.Content>
-        <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
-        <p>Temprature: {weatherData.main.temp} &deg;C</p>
-        <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-        <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-        <p>Description: {weatherData.weather[0].main}</p>
-        <p>Humidity: {weatherData.main.humidity} %</p>
-        <p>Day: {moment().format('dddd')}</p>
-        <p>Date: {moment().format('LL')}</p>
-    </Card.Content>
-  </Card>
-)
-
-export default CardExampleCard;
+    <div className="main">
+        <p className="header">{weatherData.name}</p>
+        <div className="flex">
+          <p className="day">Day: {moment().format('dddd')}</p>
+        </div>
+  
+        <div className="flex">
+          <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+        </div>
+        
+    </div>
+  )
+  
+  export default CardExampleCard;
